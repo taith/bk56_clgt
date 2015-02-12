e569789c5e619b81e5cd5848341939692798e9fc
oximenvn <oximenvn@gmail.com>
2015-02-12 02:17:30 +0000

    Add static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  root to: "home#index"
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
